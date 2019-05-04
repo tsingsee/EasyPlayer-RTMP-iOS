a237fc47a6166c57494ad3a60c0ef1f64b1e396c
liyy <1290374862@qq.com>
2019-05-04 21:46:20 +0800

    更新版本信息

--- EasyPlayer/ViewController/Setting/AboutViewController.m
+++ EasyPlayer/ViewController/Setting/AboutViewController.m
@@ -8,10 +8,12 @@
 
 #import "AboutViewController.h"
 #import "NSUserDefaultsUnit.h"
+#import "WebViewController.h"
 
 @interface AboutViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descLabel;
 
 @end
 
@@ -52,6 +54,53 @@
     
     self.nameLabel.attributedText = attr;
     self.nameLabel.numberOfLines = 0;
+
+    NSString *html = @"EasyPlayer RTMP iOS版 播放器是由 TSINGSEE青犀开放平台 开发和维护的一个完善的RTMP流媒体播放器项目，视频编码支持H.264（后续会扩展H.265），音频支持AAC，支持硬解码，软解码，是一套极佳的安防流媒体平台播放组件！EasyPlayer-RTMP iOS版本经过了很多年的发展和迭代，已经非常稳定、完整，功能包括：直播、录像、抓图，支持指令集包括arm64、armv7，应该说是目前市面上功能性、稳定性、低延时性和完整性最强的一款RTMP播放器！";
+    NSData *data = [html dataUsingEncoding:NSUnicodeStringEncoding];
+    
+    NSDictionary *options = @{ NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType,
+                               NSCharacterEncodingDocumentAttribute : @(NSUTF8StringEncoding) };
+    
+    // 设置富文本
+    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithData:data options:options documentAttributes:nil error:nil];
+    
+    // 设置段落格式
+    NSMutableParagraphStyle *para = [[NSMutableParagraphStyle alloc] init];
+    para.lineSpacing = 7;
+    para.paragraphSpacing = 10;
+    [attrStr addAttribute:NSParagraphStyleAttributeName value:para range:NSMakeRange(0, attrStr.length)];
+    
+    [attrStr addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:NSMakeRange(0, attrStr.length)];
+    [attrStr addAttribute:NSForegroundColorAttributeName value:UIColorFromRGB(0x4c4c4c) range:NSMakeRange(0, attrStr.length)];
+    
+    self.descLabel.attributedText = attrStr;
+}
+
+- (IBAction)easyDarwin:(id)sender {
+    UIButton *btn = (UIButton *)sender;
+    
+    WebViewController *controller = [[WebViewController alloc] init];
+    controller.title = @"EasyDarwin开源流媒体服务器";
+    controller.url = btn.titleLabel.text;
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
+- (IBAction)easyDSS:(id)sender {
+    UIButton *btn = (UIButton *)sender;
+    
+    WebViewController *controller = [[WebViewController alloc] init];
+    controller.title = @"EasyDSS商用流媒体解决方案";
+    controller.url = btn.titleLabel.text;
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
+- (IBAction)easyNVR:(id)sender {
+    UIButton *btn = (UIButton *)sender;
+    
+    WebViewController *controller = [[WebViewController alloc] init];
+    controller.title = @"EasyNVR无插件直播方案";
+    controller.url = btn.titleLabel.text;
+    [self.navigationController pushViewController:controller animated:YES];
 }
 
 @end
